26bdef5fddc3b5e0c1d2611ea484e068f90c5b6e
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-03-12 12:19:40 +0100

    latest push

--- pages/WatchList.jsx
+++ pages/WatchList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../Styles/WatchList.css";
+import "../Styles/Watchlist.css";
 
 const WatchlistPage = ({ token }) => {
   const [watchlist, setWatchlist] = useState([]);
