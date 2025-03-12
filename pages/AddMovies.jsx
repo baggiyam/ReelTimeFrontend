fab08b141aeddf104c5268e3dcc2dcfd29ecf831
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-03-12 12:16:24 +0100

    latest push

--- pages/AddMovies.jsx
+++ pages/AddMovies.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../Styles/MovieList.css"; // Using the existing movie list CSS
-
+import "../Styles/movielist.css"
 const AddMoviePage = ({ token }) => {
     const [formData, setFormData] = useState({
         title: "",
